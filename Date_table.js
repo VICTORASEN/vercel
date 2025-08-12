be122bf60d25ccfa743905971cef7b08d7f76c62
h-hr <106217548+h-hr@users.noreply.github.com>
2025-08-12 15:43:50 +0300

    Update Date_table.js

--- Date_table.js
+++ Date_table.js
@@ -1,4 +1,4 @@
-
+/*
 
 function get_date(day,month,year=2000){
 
@@ -9,7 +9,7 @@ let a=[year%4,year%100,year%400];
 
 
 
-/*return feb_max*/
+
 
 
 
@@ -18,22 +18,12 @@ months=Object.assign({'JANUARY':31,'MARCH':31,'APRIL':30,'MAY':31,'JUNE':30,'JUL
 months2=[31,a.includes(0)||a.join('')=='000'?29:28,31,30,31,30,31,31,30,31,30,31];
 
 console.log(JSON.stringify(months2));
-/* 
-1/1/1 
 
-1%7 = 1
-
-*/
 
 days=['SUNDAY','MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY'];
 
 28%7;
 
-/*DEC 31 1999*/
-
-/*[499, 19, 4, 15, 484, 5490]*/
-
-/*return*/
 
 function next(e,s){return(e.JANUARY+s+months.JANUARY)%7}
 
@@ -118,20 +108,17 @@ OCTOBER=((k+get(8))%7);
 NOVEMBER=((k+get(9))%7);
 DECEMBER=((k+get(10))%7);
 
+[k%7].concat(Array(11).fill(0).map((e,i)=>((k+get(i))%7)))
+
+console.log(JSON.parse(JSON.stringify({a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,JANUARY,FEBRUARY,MARCH,APRIL,MAY,JUNE,JULY,AUGUST,SEPTEMBER,OCTOBER,NOVEMBER,DECEMBER,months})))
 
 
 
-
-
-JSON.parse(JSON.stringify({a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,JANUARY,FEBRUARY,MARCH,APRIL,MAY,JUNE,JULY,AUGUST,SEPTEMBER,OCTOBER,NOVEMBER,DECEMBER,months}))
 
 
 
 
 //[31,29,31,30,31,30,31,31,30,31,30,31].slice(-(0+1)).reduce((e,e2)=>e+e2)
-
-
-function get_month(index=0){return Object.assign(document.createElement('span'),{id:'m'+index,className:'calendar',innerHTML:`<div class="c m">${['January','February','March','April','May','June','July','August','September','October','November','December'][index]}</div>`+['SUN','MON','TUE','WED','THU','FRI','SAT'].map(e=>Object.assign(document.createElement('div'),{className:'c d',innerHTML:e}).outerHTML).join('')+'<div class="c"></div>'.repeat(42)}).outerHTML}
 
 
 //document.body.innerHTML=get_month();
@@ -144,8 +131,47 @@ function get_month(index=0){return Object.assign(document.createElement('span'),
 //m1.outerHTML=get_month(1);
 
 
+
+
+*/
+
+function get_month(index=0){
+    return Object.assign(document.createElement('span'),{id:'m'+index,className:'calendar',innerHTML:`<div class="c m">${['January','February','March','April','May','June','July','August','September','October','November','December'][index]}</div>`+['SUN','MON','TUE','WED','THU','FRI','SAT'].map(e=>Object.assign(document.createElement('div'),{className:'c d',innerHTML:e}).outerHTML).join('')+'<div class="c"></div>'.repeat(42)}).outerHTML
+}
+
+
+a=2025;
+ b=a-1; c=parseInt(b/4);
+ d=parseInt(b/100);
+ e=parseInt(b/400);
+ f=c+e-d;
+ g=b-f;
+ h=f*366;
+ i=g*365;
+ j=h+i;
+ k=j+1;
+ l=k%7;
+ m=a%4;
+ n=a%100;
+ o=a%400;
+ p=[m,n,o].includes(0)?29:28;
+ months=[31,p,31,30,31,30,31,31,30,31,30,31];
+ function get(index){return months.slice(0,index+1).reduce((e,e2)=>e+e2)};
+
+day_start=[k%7].concat(Array(11).fill(0).map((e,i)=>((k+get(i))%7)));
+
+function month_start(index){day_start[index]}
+
+
+
+
+
+
+
+
 Array(12).fill('m').map((e,i)=>document.getElementById(e+i).outerHTML=get_month(i));
 
 
 
    
+
