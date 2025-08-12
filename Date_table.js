8d88adc7d47e2f86465c97714fe21019f0cbf399
h-hr <106217548+h-hr@users.noreply.github.com>
2025-08-12 16:09:58 +0300

    Update Date_table.js

--- Date_table.js
+++ Date_table.js
@@ -139,7 +139,6 @@ function get_month(index=0){
     return Object.assign(document.createElement('span'),{id:'m'+index,className:'calendar',innerHTML:`<div class="c m">${['January','February','March','April','May','June','July','August','September','October','November','December'][index]}</div>`+['SUN','MON','TUE','WED','THU','FRI','SAT'].map(e=>Object.assign(document.createElement('div'),{className:'c d',innerHTML:e}).outerHTML).join('')+'<div class="c"></div>'.repeat(42)}).outerHTML
 }
 
-
 function display_numbers(a=2025){generate_tables();let b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,months,day_start;
  b=a-1;
  c=parseInt(b/4);
@@ -158,21 +157,15 @@ function display_numbers(a=2025){generate_tables();let b,c,d,e,f,g,h,i,j,k,l,m,n
  p=[m,n,o].includes(0)?29:28;
  months=[31,p,31,30,31,30,31,31,30,31,30,31];
 
+function get(index){return months.slice(0,index+1).reduce((e,e2)=>e+e2)};
+                                 
 day_start=[k%7].concat(Array(11).fill(0).map((e,i)=>((k+get(i))%7)));
 
 console.log(JSON.parse(JSON.stringify({a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,months,day_start})));
 
-
 Array.from(Array(12).fill('m').map((e,i)=>document.getElementById(e+i))).map(e=>Array.from(e.children)).forEach((e,i)=>e.slice(8).slice(day_start[i]).slice(0,months[i]).forEach((e,i)=>e.innerHTML=i+1))
 
 }
-
- function get(index){return months.slice(0,index+1).reduce((e,e2)=>e+e2)};
-
-
-
-function month_start(index){day_start[index]}
-
 
 function generate_tables(){Array(12).fill('m').map((e,i)=>document.getElementById(e+i).outerHTML=get_month(i));}
 
@@ -185,6 +178,7 @@ display_numbers();
 
 
    
+
 
 
 
