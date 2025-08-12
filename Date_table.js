d5d55b42baaf5afe2d9ca4776a5667d5d1010baa
h-hr <106217548+h-hr@users.noreply.github.com>
2025-08-12 15:59:23 +0300

    Update Date_table.js

--- Date_table.js
+++ Date_table.js
@@ -140,7 +140,7 @@ function get_month(index=0){
 }
 
 
-a=2025;
+function display_numbers(a=2025){
  b=a-1; c=parseInt(b/4);
  d=parseInt(b/100);
  e=parseInt(b/400);
@@ -156,6 +156,14 @@ a=2025;
  o=a%400;
  p=[m,n,o].includes(0)?29:28;
  months=[31,p,31,30,31,30,31,31,30,31,30,31];
+
+console.log(JSON.parse(JSON.stringify({a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,months})));
+
+
+Array.from(Array(12).fill('m').map((e,i)=>document.getElementById(e+i))).map(e=>Array.from(e.children)).forEach((e,i)=>e.slice(8).slice(day_start[i]).slice(0,months[i]).forEach((e,i)=>e.innerHTML=i+1))
+
+}
+
  function get(index){return months.slice(0,index+1).reduce((e,e2)=>e+e2)};
 
 day_start=[k%7].concat(Array(11).fill(0).map((e,i)=>((k+get(i))%7)));
@@ -163,15 +171,16 @@ day_start=[k%7].concat(Array(11).fill(0).map((e,i)=>((k+get(i))%7)));
 function month_start(index){day_start[index]}
 
 
+function generate_tables(){Array(12).fill('m').map((e,i)=>document.getElementById(e+i).outerHTML=get_month(i));}
+
+generate_tables();
+
+display_numbers();
 
 
-
-
-
-
-Array(12).fill('m').map((e,i)=>document.getElementById(e+i).outerHTML=get_month(i));
 
 
 
    
+
 
